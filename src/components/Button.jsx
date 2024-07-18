47c5bc61e31bc516e5eb3a15fb02c60a1f6c14e9
iamjohnhm <jhm90822@gmail.com>
2024-07-19 01:01:16 +0300

    made some changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,9 @@ import { CiUndo } from "react-icons/ci";
 
 const Button = ({icon,text}) => {
   return (
-    <div className=' cursor-pointer w-fit py-2 rounded-3xl shadow-md px-5 flex flex-row border'>
-        <div className='mr-[5px]'>{icon}</div>
-        <div className='mt-[-4px] font-medium'>{text}</div>       
+    <div className=' select-none bg-black cursor-pointer hover:-translate-y-1 active:translate-y-1 transition duration-90 w-fit py-2 rounded-3xl shadow-md px-5 flex flex-row border'>
+        <div className='mr-[5px] text-white'>{icon}</div>
+        <div className='mt-[-4px] font-medium text-white'>{text}</div>       
     </div>
   )
 }
